c75d01a8f1e375e4940cc9077c5842ff7f59556a
GpNt <ff25tj@gmail.com>
2018-08-06 16:52:22 +0300

    add sorting witch space splitter

--- src/main/java/com/alevel/webapp/MyServlet.java
+++ src/main/java/com/alevel/webapp/MyServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 @WebServlet(urlPatterns = "/ask")
 public class MyServlet extends HttpServlet {
@@ -18,9 +19,32 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse response) throws IOException {
         BufferedReader reader = req.getReader();
-        String unSortedArray = CharStreams.toString(reader);
+
+        String reqestString = CharStreams.toString(reader);
+        reqestString = reqestString.substring(1, reqestString.length() - 1);
+        String[] stringArray = reqestString.split(" ");
+
+        int[] intArray = new int[stringArray.length];
+        for (int i = 0; i < stringArray.length; i++) {
+            intArray[i] = Integer.parseInt(stringArray[i]);
+        }
+
+        Arrays.sort(intArray);
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("\"");
+        for (int i = 0; i < intArray.length; i++) {
+            sb.append(intArray[i]);
+            if (i == intArray.length) {
+                break;
+            } else sb.append(" ");
+        }
+        sb.append("\"");
+
+        String responseString = sb.toString();
 
         response.setHeader(CONTENT_TYPE, MediaType.PLAIN_TEXT_UTF_8.type());
-        response.getWriter().write(unSortedArray);
+        response.getWriter().write(responseString);
     }
 }
